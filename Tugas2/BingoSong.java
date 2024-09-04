ac733f3b2ea05566cc6a20883e5ca68044098c65
Zahra fadhilatusyifa <147789266+Fadhilatusyifa@users.noreply.github.com>
2024-09-04 09:32:45 +0700

    Add files via upload

---
+++ Tugas2/BingoSong.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Tugas2;
+
+/**
+ *
+ * @author lala_
+ */
+public class BingoSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bingo song = new Bingo();
+        song.cetakLagu();
+    }
+}
---
+++ Tugas2/InterfaceMatematika.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tugas2;
+
+/**
+ *
+ * @author lala_
+ */
+public interface InterfaceMatematika {
+   public int Pertambahan (int a, int b);
+   public int Pengurangan (int a, int b);
+   public int Perkalian (int a, int b);
+   public int Pembagian (int a, int b);
+   
+}
